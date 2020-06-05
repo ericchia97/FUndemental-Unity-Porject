05ff591cf89e26bb4b6e980f05383f0365fe5ad4
ericchia97 <ericchia508@gmail.com>
2020-06-06 02:57:01 +0800

    Fundemental skills in Unity
    This is a prototype first made and provided by PluralSight, a Unity Learning Program partner.
    
    Theis project has concluded the skills learnt, such as building environment, creating sound effects, light baking, creating mesh component, implementing player movement, applying simple animation, C# scripting, end etc.

---
+++ UnityFundemental/Assets/Scripts/NPCController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCController : MonoBehaviour {
+
+    private Animator anim;
+    private NavMeshAgent agent;
+
+    public float PatrolTime = 10f;
+    public float aggroRange = 10f;
+    public Transform[] waypoints;
+
+    private int index;
+    private float speed, agentSpeed;
+    private Transform player;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        if(agent != null) { agentSpeed = agent.speed; }
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length);
+
+        InvokeRepeating("Tick", 0, 0.5f);
+
+        if(waypoints.Length > 0)
+        {
+            InvokeRepeating("Patrol", 0, PatrolTime);
+        }
+    }
+
+    void Patrol()
+    {
+        index = index == waypoints.Length - 1 ? 0 : index + 1;
+    }
+
+    void Tick()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+        agent.destination = waypoints[index].position;
+
+        if(player != null && Vector3.Distance(transform.position, player.position) < aggroRange)
+        {
+            agent.destination = player.position;
+            agent.speed = agentSpeed / 2;
+        }
+    }
+}
---
+++ UnityFundemental/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Animator anim;
+    private NavMeshAgent agent;
+    private Transform target;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("target").transform;
+    }
+
+    void Update()
+    {
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+
+        if (target != null && Vector3.Distance(transform.position, target.position) < 3f)
+        {
+            anim.SetFloat("Attack", 0);
+        }
+    }
+}
